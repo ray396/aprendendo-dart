bf843873c50d18e264c9066bed8c1d78df6a4163
ray396 <rayssadantas31@gmail.com>
2023-01-11 11:44:57 -0300

    estudo do dart

--- 02-Operadores/Logicos.dart
+++ 02-Operadores/Logicos.dart
@@ -5,4 +5,8 @@ void main() {
   bool falso = !true;
 
   print('Verdadeiro = ${verdadeiro} falso = ${falso}');
+
+  //operador && (e) somente se ambas afirmações for true o retornor sera true
+  //operador || (ou) precisa somente de uma afirmação verdadeira para retornar true
+  //operador ! (not) inverte o valor boleano
 }
